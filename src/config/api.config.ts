35f018ca92abe02b942f6fdfeba40c1108aea1c8
felipecarvalho-dev <felipihsc@gmail.com>
2022-07-15 23:06:40 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080/"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl:"https://app-springboot-bucket.s3.sa-east-1.amazonaws.com"
+                
 }
