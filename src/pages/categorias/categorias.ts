8d854b062a56017679716286363b06369c896598
felipecarvalho-dev <felipihsc@gmail.com>
2022-08-25 22:57:33 -0300

    Carregando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -38,7 +38,7 @@ items: CategoriaDTO[];
   }
 
 
-  showProdutos(){
-    this.navCtrl.push('ProdutosPage');
+  showProdutos(categoria_id : string){
+    this.navCtrl.push('ProdutosPage', {categoria_id: categoria_id});
   }
 }
